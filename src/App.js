9bc4b28b84e1697ebcfa074b158d7da3278a0fcf
DanielVillasmil <dvillasmil08@gmail.com>
2022-05-12 12:25:58 -0400

    added props for expense items

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2022, 7, 14),
     },
     {
-      id: "e2",
-      title: "New TV",
+      id: 'e2',
+      title: 'New TV',
       amount: 799.49,
       date: new Date(2022, 2, 12),
     },
     {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2022, 2, 28),
     },
     {
-      id: "e4",
-      title: "New Wood Desk",
+      id: 'e4',
+      title: 'New Wood Desk',
       amount: 450,
       date: new Date(2022, 5, 12),
     },
