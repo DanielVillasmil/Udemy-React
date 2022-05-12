0b7ebd4254b39834c371c2e502557e7da74b517b
DanielVillasmil <dvillasmil08@gmail.com>
2022-05-12 11:33:45 -0400

    added styling to expense item as a whole

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className='expense-item'>
             <div>March 28th 2022</div>
-                <div>
+                <div className='expense-item__description'>
                     <h2>Car Insurance</h2>
                 </div>
-            <div>$294.67</div>
+            <div className='expense0-item__price'>$294.67</div>
         </div>
     );
 }
