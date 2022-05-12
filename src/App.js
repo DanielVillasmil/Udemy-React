daa54f76922ed25bb788b26b3aea7a386e530adf
DanielVillasmil <dvillasmil08@gmail.com>
2022-05-12 11:59:01 -0400

    created expenses array

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+  ]
+
   return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem />
       <ExpenseItem />
       <ExpenseItem />
       <ExpenseItem />
