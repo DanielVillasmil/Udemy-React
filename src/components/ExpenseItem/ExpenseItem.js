8f6258d82c656bb60f5e2301f44f231ab5d54f79
DanielVillasmil <dvillasmil08@gmail.com>
2022-05-12 11:41:54 -0400

    created date object as plain JS

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,6 +1,8 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 28);
+
     return (
         <div className='expense-item'>
             <div>March 28th 2022</div>
