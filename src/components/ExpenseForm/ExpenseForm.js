c0809d4bedf142ab4334706c32137ec254adb906
Daniel <dvillasmil08@gmail.com>
2022-06-15 10:22:49 -0400

    tested in web console, data is passing

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -19,25 +19,51 @@ const ExpenseForm = () => {
     const dateChangeHanlder = (event) => {
         setEnteredDate(event.target.value);
     };
-    return <form>
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date (enteredDate)
+    };
+
+    console.log(expenseData);
+
+
+
+    return (
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input type='text' 
+                onChange={titleChangeHandler} 
+                />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangeHanlder} />
+                <input 
+                type='number' 
+                min='0.01' 
+                step='0.01' 
+                onChange={amountChangeHanlder} />
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2030-12-31' onChange={dateChangeHanlder} />
+                <input 
+                type='date' 
+                min='2019-01-01' 
+                max='2030-12-31' 
+                onChange={dateChangeHanlder} />
             </div>
         </div>
         <div className='new-expense__actions'>
             <button type='submit'>Add Expense</button>
         </div>
     </form>
-};
+)};
 
 export default ExpenseForm;
