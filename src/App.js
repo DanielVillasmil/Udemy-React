3b78511dd26d1bb82ff60c577a3fe9025c36c036
Daniel <dvillasmil08@gmail.com>
2022-06-15 10:47:47 -0400

    added expense handler in App.js

--- src/App.js
+++ src/App.js
@@ -31,9 +31,15 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expenses)
+  }
+
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
