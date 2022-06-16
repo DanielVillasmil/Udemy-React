2d11b31f60874c7c7b6e84b673b50d73e2c1e783
Daniel <dvillasmil08@gmail.com>
2022-06-16 19:32:18 -0400

    added more years to year dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import './Expenses.css';
 import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2022');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
@@ -16,7 +16,10 @@ const Expenses = (props) => {
   return (
       <div>
       <Card className='expenses'>
-        <ExpenseFilter onChangeFilter={filterChangeHandler}/>
+        <ExpenseFilter 
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+        />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
