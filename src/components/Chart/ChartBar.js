18c50530200a3d343a8ba88553a34aa6d14f5291
dvillasmil08 <dvillasmil08@gmail.com>
2022-09-08 08:57:49 -0400

    addded chart logic

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,16 @@
+import './CharBar.css';
+
+const CharBar = props => {
+    return <div className='chart-bar'>
+        <div className='chart-bar__inner'>
+            <div className='chart-bar__fill'>
+
+            </div>
+        </div>
+        <div  className='chart-bar__label'>
+            
+        </div>
+    </div>
+};
+
+export default CharBar;
